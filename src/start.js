a78c93ae7e32da990653622e9a63dcaef3ad6360
Tahir J <icodejs@gmail.com>
2017-01-03 21:32:48 +0000

    Refine Footy Addicts "has space function"

--- src/start.js
+++ src/start.js
@@ -1,16 +1,31 @@
 import request from 'request-promise';
 import cheerio from 'cheerio';
 
-const url = 'https://footyaddicts.com/football-games/18741-5-a-side-football-playfootball-shepherds-bush-london';
+const url = 'https://footyaddicts.com/football-games/19308-9-a-side-football-westway-sports-centre-london';
 
 request({ url })
   .then(cheerio.load)
   .then($ => {
-    return $('.player').map((index, el) => {
-      return {
-        name: $(el).find('a').attr('title')
-      }
+    const $players = $('.player');
+    const count = $players.length;
+    const players = $players.map((index, el) => {
+      return $(el).find('a').attr('title');
     }).toArray();
+
+    return { count, players }
+  })
+  .then(({ count, players }) => {
+    const duds = [
+      'PlayFootball Staff',
+      'Koulis Papadopoulos',
+      'Kos Den',
+      'Kos Den\'s',
+      'Goran Dravic',
+      undefined
+    ];
+
+    const spaces = count - players.filter(p => duds.indexOf(p) > -1).length;
+    return spaces;
   })
   .then(console.log)
   .catch(console.err);
