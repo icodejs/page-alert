1cf5cb90bc722fac17fcdbc75bdef5d57523aefc
Tahir J <icodejs@gmail.com>
2017-01-05 20:53:45 +0000

    Send email if 1 space is left

--- src/start.js
+++ src/start.js
@@ -1,7 +1,11 @@
 import request from 'request-promise';
+import nodemailer from 'nodemailer';
 import cheerio from 'cheerio';
+import optimist from 'optimist';
 
-const url = 'https://footyaddicts.com/football-games/18778-5-a-side-football-playfootball-shepherds-bush-london';
+const { argv } = optimist;
+const [ url, email, password, recipient ] = argv._;
+const transporter = nodemailer.createTransport(`smtps://${email}:${password}@smtp.gmail.com`);
 
 request({ url })
   .then(cheerio.load)
@@ -28,9 +32,39 @@ request({ url })
       'open',
       undefined
     ];
-
-    const squad = players.filter(p => duds.indexOf(p) === -1).length;
-    return numbersAside - squad;
+    return numbersAside - players.filter(p => duds.indexOf(p) === -1).length;
   })
-  .then(console.log) // if one space left, send an email
+  .then(space => {
+    if (space !== 1) {
+      return;
+    }
+
+    return {
+      from: '"Page Alerts" <page-alerts@noreply.com>',
+      to: recipient || email,
+      subject: 'Footy Addicts - Last Space',
+      html: `<p>There's ${space} space available at: <a href="${url}">${url}</a></p>`
+    };
+  })
+  .then(options => {
+    return new Promise((resolve, reject) => {
+      if (!options) {
+        return resolve('Do nothing');
+      }
+
+      transporter.sendMail(options, (error, info) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve('Message sent: ' + info.response);
+      });
+    });
+  })
+  .then(console.log)
   .catch(console.err);
+
+// e.g.
+// npm start https://footyaddicts.com/football-games/18778-5-a-side-football-playfootball-shepherds-bush-london icodejs@gmail.com passw
+
+// Notes
+// Send via gmail: https://www.google.com/settings/security/lesssecureapps
